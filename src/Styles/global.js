75255d96adabe548f9e832390a74cd9bf7c0a8e9
Abhishek Kumar <abhishek29112003@email.com>
2026-09-02 01:28:08 +0530

    done changes

---
+++ src/Styles/global.js
@@ -0,0 +1,14 @@
+import{createGlobalStyle} from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle
+
+`
+body{
+    background-color: #00dbeb;
+    }
+.test{
+    font-size: 5rem;
+
+
+
+}`
